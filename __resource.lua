7dc86aca1db11f68f25ee5675d3dd910cf18bc1b
pongo1231 <pongo1999712@gmail.com>
2017-01-25 20:01:23 +0100

    Added

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+client_scripts
+{
+	"vehicle/enterexit.lua",
+	"vehicle/health.lua",
+	"vehicle/horn.lua",
+	"vehicle/drive.lua",
+
+	"player/spawn.lua",
+	"player/health.lua",
+	"player/model.lua",
+	"player/moving.lua",
+}
